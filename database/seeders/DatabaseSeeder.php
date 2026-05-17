c114a7e96188cc59edc9fe7609020100674edefc
isamee117-rgb <isamee117@gmail.com>
2026-05-17 15:42:39 +0500

    1fix: json column default value for mysql 8

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 use App\Models\Patient;
 use App\Models\Doctor;
 use App\Models\Floor;
@@ -21,6 +22,14 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        User::create([
+            'name'     => 'Super Admin',
+            'email'    => 'Superadmin@healthops.com',
+            'password' => bcrypt('Superadmin@54321'),
+            'role'     => 'superadmin',
+            'is_active' => true,
+        ]);
+
         HospitalInfo::create([
             'name' => 'Nova Medical Complex',
             'short_name' => 'Nova HMS',
